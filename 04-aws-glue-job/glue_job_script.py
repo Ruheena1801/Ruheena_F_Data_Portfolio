852aa4466039b48ed0e297bca049de6345f1b865
Ruheena1801 <ruheenafatima01@gmail.com>
2026-08-17 13:59:30 -0700

    Expand AWS Glue pipeline sample

--- 04-aws-glue-job/glue_job_script.py
+++ 04-aws-glue-job/glue_job_script.py
@@ -1,29 +1,14 @@
+# AWS Glue ETL Pipeline (portfolio sample)
+# Demonstrates incremental RDS extraction, PySpark transformations,
+# data-quality checks, and partitioned Parquet output to S3.
+
 import sys
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as F
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
-
-jdbc_url = "jdbc:postgresql://rds-endpoint:5432/appdb"
-props = {"user":"user","password":"***","driver":"org.postgresql.Driver"}
-
-df = spark.read.jdbc(url=jdbc_url, table="public.orders", properties=props)
-df = df.dropDuplicates(["order_id"]).withColumnRenamed("created_at", "event_ts")
-
-glueContext.write_dynamic_frame.from_options(
-    frame=DynamicFrame.fromDF(df, glueContext, "df"),
-    connection_type="s3",
-    connection_options={"path": "s3://bucket/bronze/orders/"},
-    format="parquet"
-)
-
-job.commit()
+# -----------------------------------------------------------------------------
+# Job parameters
+# LAST_WATERMARK represents the latest
